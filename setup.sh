812664aa33922ed10d1ae70f78f8eba3fda1cab7
dluongo9 <54182801+dluongo9@users.noreply.github.com>
2022-05-18 17:16:37 -0700

    changes setup.sh to not use a yml

--- setup.sh
+++ setup.sh
@@ -7,13 +7,19 @@ else
   source $1
 fi
 
-#conda create --prefix ./envs
-#echo 'installing bob.bio.spear'
-#conda activate ./envs
-#conda install \
-#-c https://www.idiap.ch/software/bob/conda \
-#-c defaults \
-#-c https://www.idiap.ch/software/bob/conda/label/archive \
-#bob=4.0.0 bob.bio.spear
+conda create --prefix ./envs
+echo 'installing bob.bio.spear'
+conda activate ./envs
+conda install \
+-c https://www.idiap.ch/software/bob/conda \
+-c defaults \
+-c https://www.idiap.ch/software/bob/conda/label/archive \
+bob=4.0.0 bob.bio.spear
 
-conda env create --prefix ./envs --file environment_droplet.yml
+pip install pydub
+conda install ffmpeg
+pip install ffprobe
+pip install ffmpeg
+
+
+#conda env create --prefix ./envs --file environment_droplet.yml
