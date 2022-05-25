7d3aea1add2889762d401e3e7d79294d9cc281ad
dluongo9 <54182801+dluongo9@users.noreply.github.com>
2022-05-25 15:57:29 -0700

    put create_database.py in the repo

--- config/create_database.py
+++ config/create_database.py
@@ -22,39 +22,39 @@ def main():
 def make_lst():
     for lang in LANGS:
         if lang == 'russian':  # TODO take out
-            dir = os.path.join('untarred', lang, LANGS[lang])
+            dir = os.path.join('../databases/corpora/untarred', lang, LANGS[lang])
 
             validated = pd.read_csv(os.path.join(dir, 'invalidated.tsv'), sep='\t', header=0)
             print(len(validated['client_id'].unique()), f'voices in {lang}')
 
-            with open('../toy_database/norm/train_world.lst', 'w') as file:  # TODO update in-line with folder/file from FOLDERS
+            with open('../../databases/toy_database/norm/train_world.lst', 'w') as file:  # TODO update in-line with folder/file from FOLDERS
                 writer = []
                 for i in range(len(validated['path']))[:200]:  # slice to shorten
                     path, client_id = validated['path'][i][:-4], validated['client_id'][i]
                     writer.append(path + '\t' + client_id)
                 file.write('\n'.join(writer))
 
-            with open('../toy_database/dev/for_models.lst', 'w') as file:
+            with open('../../databases/toy_database/dev/for_models.lst', 'w') as file:
                 writer = []
                 for i in range(len(validated['path']))[200:230]:
                     path, client_id = validated['path'][i][:-4], validated['client_id'][i]
                     writer.append(path + '\t' + client_id + '\t' + client_id)
                 file.write('\n'.join(writer))
 
-            with open('../toy_database/dev/for_probes.lst', 'w') as file:
+            with open('../../databases/toy_database/dev/for_probes.lst', 'w') as file:
                 writer = []
                 for i in range(len(validated['path']))[230:260]:
                     path, client_id = validated['path'][i][:-4], validated['client_id'][i]
                     writer.append(path + '\t' + client_id)
                 file.write('\n'.join(writer))
 
-            with open('../toy_database/eval/for_models.lst', 'w') as file:
+            with open('../../databases/toy_database/eval/for_models.lst', 'w') as file:
                 writer = []
                 for i in range(len(validated['path']))[260:290]:
                     path, client_id = validated['path'][i][:-4], validated['client_id'][i]
                     writer.append(path + '\t' + client_id + '\t' + client_id)
                 file.write('\n'.join(writer))
-            with open('../toy_database/eval/for_probes.lst', 'w') as file:
+            with open('../../databases/toy_database/eval/for_probes.lst', 'w') as file:
                 writer = []
                 for i in range(len(validated['path']))[290:320]:
                     path, client_id = validated['path'][i][:-4], validated['client_id'][i]
