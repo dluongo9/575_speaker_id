2fffe7520c0b0d60f7dd0f99a5362927f7fb09a4
akiliyer <emailakiliyer@gmail.com>
2022-05-27 12:27:52 -0700

    update sound file lengths with time echos, create cmd

--- src/sound_file_length.sh
+++ src/sound_file_length.sh
@@ -1,15 +1,33 @@
 #!/bin/sh
 
+echo dv lengths
+start="date +%s"
+cd ../../databases/corpora/untarred/dv/clips/
+for file in *.mp3
+do
+  duration=$(ffprobe "$file" 2>&1 | awk '/Duration/ { print $2 }')
+  echo -e "$duration\t$file"
+  #echo -e "$file"
+done > ../../../../../575_speaker_id/config/dv_len.txt
+end="date +%s"
+runtime=$((end-start))
+echo dv runtime: $runtime
+
 echo ru lengths
-cd ../../databases/corpora/untarred/ru/clips/
+start="date +%s"
+cd ../../ru
 for file in *.mp3
 do
   duration=$(ffprobe "$file" 2>&1 | awk '/Duration/ { print $2 }')
   echo -e "$duration\t$file"
   #echo -e "$file"
 done > ../../../../../575_speaker_id/config/ru_len.txt
+end="date +%s"
+runtime=$((end-start))
+echo ru runtime: $runtime
 
 echo ta lengths
+start="date +%s"
 cd ../../ta
 for file in *.mp3
 do
@@ -17,12 +35,6 @@ do
   echo -e "$duration\t$file"
   #echo -e "$file"
 done > ../../../../../575_speaker_id/config/ta_len.txt
-
-echo dv lengths
-cd ../../dv
-for file in *.mp3
-do
-  duration=$(ffprobe "$file" 2>&1 | awk '/Duration/ { print $2 }')
-  echo -e "$duration\t$file"
-  #echo -e "$file"
-done > ../../../../../575_speaker_id/config/dv_len.txt
+end="date +%s"
+runtime=$((end-start))
+echo ta runtime: $runtime
