d81ea50ca70047577539d9d2ba0748c40b51a131
akiliyer <emailakiliyer@gmail.com>
2022-05-27 12:02:12 -0700

    Changes by laurel

--- src/sound_file_length.sh
+++ src/sound_file_length.sh
@@ -6,4 +6,20 @@ do
   duration=$(ffprobe "$file" 2>&1 | awk '/Duration/ { print $2 }')
   echo -e "$duration\t$file"
   #echo -e "$file"
-done
+done > ../../../../../575_speaker_id/config/ru_len.txt
+
+cd ../../ta
+for file in *.mp3
+do
+  duration=$(ffprobe "$file" 2>&1 | awk '/Duration/ { print $2 }')
+  echo -e "$duration\t$file"
+  #echo -e "$file"
+done > ../../../../../575_speaker_id/config/ta_len.txt
+
+cd ../../dv
+for file in *.mp3
+do
+  duration=$(ffprobe "$file" 2>&1 | awk '/Duration/ { print $2 }')
+  echo -e "$duration\t$file"
+  #echo -e "$file"
+done > ../../../../../575_speaker_id/config/dv_len.txt
