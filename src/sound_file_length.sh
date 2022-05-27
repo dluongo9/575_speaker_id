727a9ead806390d3cc97f6c9574038c9998cf3ea
dluongo9 <54182801+dluongo9@users.noreply.github.com>
2022-05-27 11:42:36 -0700

    sound_file_length script added

---
+++ src/sound_file_length.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+for file in ../../databases/corpora/untarred/ru/clips/*.mp3
+do
+  duration=$(ffprobe "$file" 2>&1 | awk '/Duration/ { print $2 }')
+  echo -e "$duration\t$file"
+  #echo -e "$file"
+done
