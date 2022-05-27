5ab19a0e60c4e65ffd27492cd8f622e567bc64be
akiliyer <emailakiliyer@gmail.com>
2022-05-27 12:02:06 -0700

    update sound_file_length.sh for all langs

--- src/sound_file_length.sh
+++ src/sound_file_length.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-for file in ../../databases/corpora/untarred/ru/clips/*.mp3
+cd ../../databases/corpora/untarred/ru/clips/
+for file in *.mp3
 do
   duration=$(ffprobe "$file" 2>&1 | awk '/Duration/ { print $2 }')
   echo -e "$duration\t$file"
