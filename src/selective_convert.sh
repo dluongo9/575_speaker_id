d4f6b01e804e92caa865ab2d37c41e79c3ed9405
dluongo9 <54182801+dluongo9@users.noreply.github.com>
2022-05-30 14:42:31 -0700

    did a bunch of stuff.

---
+++ src/selective_convert.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+ffmpeg -i "../../databases/corpora/untarred/ru/clips/common_voice_ru_25792615.mp3" "../../databases/corpora/untarred/ru/wav/common_voice_ru_25792615.wav" -n
+ffmpeg -i "../../databases/corpora/untarred/ru/clips/common_voice_ru_26587318.mp3" "../../databases/corpora/untarred/ru/wav/common_voice_ru_26587318.wav" -n
+ffmpeg -i "../../databases/corpora/untarred/ru/clips/common_voice_ru_28394513.mp3" "../../databases/corpora/untarred/ru/wav/common_voice_ru_28394513.wav" -n
+ffmpeg -i "../../databases/corpora/untarred/ru/clips/common_voice_ru_31251953.mp3" "../../databases/corpora/untarred/ru/wav/common_voice_ru_31251953.wav" -n
+ffmpeg -i "../../databases/corpora/untarred/ru/clips/common_voice_ru_30674299.mp3" "../../databases/corpora/untarred/ru/wav/common_voice_ru_30674299.wav" -n
+ffmpeg -i "../../databases/corpora/untarred/ru/clips/common_voice_ru_30387504.mp3" "../../databases/corpora/untarred/ru/wav/common_voice_ru_30387504.wav" -n
+ffmpeg -i "../../databases/corpora/untarred/ru/clips/common_voice_ru_19468869.mp3" "../../databases/corpora/untarred/ru/wav/common_voice_ru_19468869.wav" -n
+ffmpeg -i "../../databases/corpora/untarred/ru/clips/common_voice_ru_27709746.mp3" "../../databases/corpora/untarred/ru/wav/common_voice_ru_27709746.wav" -n
