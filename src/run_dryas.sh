a7729dfdcaa153d06b948f18793ec0efc618abf3
dluongo9 <54182801+dluongo9@users.noreply.github.com>
2022-05-30 20:23:08 -0700

    FINAL VERSION

---
+++ src/run_dryas.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+echo create database
+cd ../config
+python create_multilingual_db.py
+
+echo verify experiment ru
+cd ../src
+verify.py ubm_ru_config.py
+
+echo verify experiment ta
+cd ../src
+verify.py ubm_ta_config.py
+
+echo collect results ru
+collect_results.py -vv --directory ../output/ru/ --sort -o ../results/ru/collect_results_ru.txt
+
+echo collect results ta
+collect_results.py -vv --directory ../output/ta/ --sort -o ../results/ta/collect_results_ta.txt
+
+echo evaluate ru
+evaluate.py -d ../output/ru/output/ru/None/nonorm/scores-dev -e ../output/ru/output/ru/None/nonorm/scores-eval -D ../results/ru/DET-ru.pdf -c EER > ../results/ru/evaluate_results.txt
+
+echo evaluate ta
+evaluate.py -d ../output/ta/output/ta/None/nonorm/scores-dev -e ../output/ta/output/ta/None/nonorm/scores-eval -D ../results/ta/DET-ta.pdf -c EER > ../results/ta/evaluate_results.txt
+
