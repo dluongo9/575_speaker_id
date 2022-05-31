64ee89cd93d6e9b61c9e76a12ea7e51073df99ac
dluongo9 <54182801+dluongo9@users.noreply.github.com>
2022-05-30 23:56:03 -0700

    FINAL VERSION

--- src/move_results.py
+++ src/move_results.py
@@ -13,12 +13,12 @@ os.system(f'cp ../logs/demographic_metrics.txt ../all_results/{time}/demographic
 
 # copy ru DET, evaluation and collect results files
 os.system(f'cp ../results/ru/evaluate_results.txt ../all_results/{time}/ru-evaluate_results.txt')
-os.system(f'cp ../results/ru/DET-ru.PDF ../all_results/{time}/DET-ru.pdf')
+os.system(f'cp ../results/ru/DET-ru.pdf ../all_results/{time}/DET-ru.pdf')
 os.system(f'cp ../results/ru/collect_results_ru.txt ../all_results/{time}/ru-collect_results.txt')
 
 # copy ta DET, evaluation and collect results files
 os.system(f'cp ../results/ta/evaluate_results.txt ../all_results/{time}/ta-evaluate_results.txt')
-os.system(f'cp ../results/ta/DET-ta.PDF ../all_results/{time}/DET-ta.pdf')
+os.system(f'cp ../results/ta/DET-ta.pdf ../all_results/{time}/DET-ta.pdf')
 os.system(f'cp ../results/ta/collect_results_ta.txt ../all_results/{time}/ta-collect_results.txt')
 
 # copy experiment.info files
